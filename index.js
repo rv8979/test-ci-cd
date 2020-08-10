3854a082947a800c7e1418939ce8d3951baf8b96
Raghav Chauhan <45090896+rv8979@users.noreply.github.com>
2020-08-11 01:21:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('rv rv rv')
+  response.send('rv rv rv3')
 }) 
 
 app.listen(app.get('port'), function() {
